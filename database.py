4c112fbf782c7ced339303f8eed75689c6c7acc4
avx <avx1245@gmail.com>
2026-05-13 23:01:04 +0530

    migrated from sqlite to postgresql

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "sqlite:///./books.db"
+DATABASE_URL = "postgresql://postgres:pass4321@localhost:5432/book_library"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
